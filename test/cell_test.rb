88a1fd38a5d3f0827fb404531ffa6fc543b6d14c
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-03-19 16:49:28 -0600

    Add init and attr tests for Cell class

---
+++ test/cell_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/cell'
+require './lib/ship'
+class CellTest < Minitest::Test
+  def setup
+    @cell = Cell.new("B4")
+    @ship = Ship.new("Cruiser", 3)
+  end
+
+  def test_it_exists
+    assert_instance_of Cell, @cell
+  end
+
+  def test_it_has_a_coordinate
+    assert_equal "B4", @cell.coordinate
+  end
+
+  def test_it_does_not_have_a_ship_by_default
+    assert_nil @cell.ship
+  end
+
+  def test_it_is_empty_by_default
+    assert @cell.empty?
+  end
+end
