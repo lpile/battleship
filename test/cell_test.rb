8ac535f042bf15887842240f9bb0c0dd8c643cae
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-03-19 18:22:49 -0600

    Add 'fire_upon' tests to Cell tests

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -33,4 +33,20 @@ class CellTest < Minitest::Test
     @cell.place_ship(@ship)
     refute @cell.empty?
   end
+
+  def test_it_is_not_fired_upon_by_default
+    refute @cell.fired_upon?
+  end
+
+  def test_fire_upon_reduces_a_placed_ships_health
+    @cell.place_ship(@ship)
+    @cell.fire_upon
+
+    assert_equal 2, @cell.ship.health
+  end
+
+  def test_fire_upon_updates_fired_upon_to_true
+    @cell.fire_upon
+    assert @cell.fired_upon?
+  end
 end
