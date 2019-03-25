cbb5c49dddcb6ba24126225249b7291c4084bedf
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-03-25 16:35:00 -0600

    Add random computer placement method with ship
    Co-authored-by: BrennanAyers <brennan.f.ayers@gmail.com>
    Co-authored-by: lpile <logan.pile@gmail.com>

--- lib/computer.rb
+++ lib/computer.rb
@@ -9,8 +9,20 @@ class Computer
     @board = Board.new(board_size)
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
-
+    computer_place(@cruiser)
+    computer_place(@submarine)
   end
 
+  def computer_place(ship)
+    cells = @board.cells.keys
+    all_combos = cells.combination(ship.length).to_a
+    valid_coords = []
+    all_combos.each do |combo|
+      if @board.valid_placement?(ship, combo)
+        valid_coords << combo
+      end
+    end
+    @board.place(ship, valid_coords.sample)
+  end
 
 end
